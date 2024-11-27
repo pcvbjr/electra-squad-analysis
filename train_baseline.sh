41cd58baffd5cdf767dcac160d7e8e5b71630352
pcvbjr <pcvbjr@gmail.com>
2024-11-27 16:03:36 -0500

    add training baseline script

---
+++ train_baseline.sh
@@ -0,0 +1,1 @@
+TRANSFORMERS_CACHE=../gdrive/MyDrive/MSAI Course Work/NLP/final_project/hf_cache python3 run.py --do_train --task qa --dataset squad --output_dir ../gdrive/MyDrive/MSAI Course Work/NLP/final_project/trained_model/
