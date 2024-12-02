fac0fa6ac7690570998904c045dc680dc6fafe6b
pcvbjr <pcvbjr@gmail.com>
2024-12-02 12:01:31 -0700

    debug

--- train_cartography.sh
+++ train_cartography.sh
@@ -2,6 +2,6 @@ HF_HOME="../gdrive/MyDrive/MSAI Course Work/NLP/final_project/hf_cache" python3 
 --do_train \
 --task qa \
 --dataset squad \
---output_dir "../gdrive/MyDrive/MSAI Course Work/NLP/final_project/trained_model_collect_cartography_5e/" \
---per_device_train_batch_size 128
+--output_dir "../gdrive/MyDrive/MSAI Course Work/NLP/final_project/trained_model_collect_cartography_5epochs/" \
+--per_device_train_batch_size 128 \
 --num_train_epochs 5
