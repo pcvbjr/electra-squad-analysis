abb3beb2597475a5271a4fae9f352d9b94b004b8
pcvbjr <pcvbjr@gmail.com>
2024-12-02 10:56:28 -0700

    extend training to 5 epochs

--- train_cartography.sh
+++ train_cartography.sh
@@ -4,3 +4,4 @@ HF_HOME="../gdrive/MyDrive/MSAI Course Work/NLP/final_project/hf_cache" python3 
 --dataset squad \
 --output_dir "../gdrive/MyDrive/MSAI Course Work/NLP/final_project/trained_model_collect_cartography/" \
 --per_device_train_batch_size 128
+--num_train_epochs 5
